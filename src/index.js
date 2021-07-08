219d9d9668c7b20ae0ed7e7b81d8b21f46f020d2
Katy La Tour <klatour324@gmail.com>
2021-07-08 14:34:07 -0500

    Add Auth0Provider and oauth configurations

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Auth0Provider
+    domain="dev-jjgf5zq2.us.auth0.com"
+    clientId="A3Zo1bv45OhHnUd9pl1aTWwgSZR8VMLG">
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
